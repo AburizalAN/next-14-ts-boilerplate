b46361c74bb763a751d62b014c28c30da3e9f9a1
aburizal nugroho <aburizal853@gmail.com>
2024-03-31 10:24:56 +0700

    update packages

--- features/general/views/HomePage/index.tsx
+++ features/general/views/HomePage/index.tsx
@@ -1,11 +1,12 @@
 import dictionaries from "@app/features/general/dictionaries"
-import { useAppSelector } from "@app/utils/redux_utils/hooks"
 import TestComponent from "./TestComponent"
+import { Button } from "@app/components/ui/button"
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen">
       <h1 className="text-2xl font-semibold">{dictionaries.helloWorld}</h1>
+      <div><Button>Test Button</Button></div>
       <TestComponent />
     </main>
   )
